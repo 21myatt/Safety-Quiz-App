f2b2f1097d6d9f5c4947fccd6e9b7df485f2235b
21Myatt <36847445+21myatt@users.noreply.github.com>
2019-07-23 05:03:28 +0630

    Create end.js

---
+++ end.js
@@ -0,0 +1,23 @@
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+result = (mostRecentScore / 50) * 100;
+
+
+const congratulationText = document.getElementById("Congratulation");
+const passText = document.getElementById("pass");
+const homeText = document.getElementById("home");
+
+finalScore.innerText = `${result}% corrected`;
+
+if (result >=50) {
+  congratulationText.innerText = `Congratulation!!!`;
+  passText.innerText = `You pass the Safety Test <3`;
+  homeText.innerText = `Go home`;
+  finalScore.style.color = '#05c46b';
+}
+else {
+  congratulationText.innerText = `Try Next Time!!!`;
+  passText.innerText = `You are pretty good, Try next time dude`;
+  homeText.innerText = `Try Again`;
+  finalScore.style.color = '#ff3f34';
+}
